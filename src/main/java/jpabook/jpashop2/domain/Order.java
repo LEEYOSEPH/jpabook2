d4879e5d5990ad9735703d2586807c74f88945a6
LEEYOSEPH <laksmi0628@gmail.com>
2022-06-21 20:18:26 +0900

    기본 매핑

---
+++ src/main/java/jpabook/jpashop2/domain/Order.java
@@ -0,0 +1,21 @@
+package jpabook.jpashop2.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
